e5d22479b59b9e0f3bf20b1987a837b9f07719e7
shivam <shivam.ultivic@gmail.com>
2023-06-16 20:04:03 +0530

    commented the write code

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -76,8 +76,8 @@ export default async function handler(req, res) {
 
   const xml = generateSitemapXML(pathsArray);
 
-  const filePath = path.join(process.cwd(), 'public', 'sitemap.xml');
-  fs.writeFileSync(filePath, xml);
+//   const filePath = path.join(process.cwd(), 'public', 'sitemap.xml');
+//   fs.writeFileSync(filePath, xml);
 
   res.setHeader('Content-Type', 'text/xml');
   res.send(xml);
